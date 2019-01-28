e344884d96ca42b8a7e7670a013ded7446b66820
lee-forde <46717325+lee-forde@users.noreply.github.com>
2019-01-28 21:48:50 +0000

    trying

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Rigidbody2D rb2d;
+
+    public HingeJoint2D HingeTrap;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.gameObject.name == "Hero")
+        {
+            RigidbodyType2D.Kinematic
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 }
