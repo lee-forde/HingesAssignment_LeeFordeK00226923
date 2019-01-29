3ed23e0933574334701a01938d9b863a0653fbaa
lee-forde <46717325+lee-forde@users.noreply.github.com>
2019-01-29 02:44:40 +0000

    Finished!

---
+++ Assets/TrapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public Rigidbody2D HingeTrap;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Hero")
+        {
+            HingeTrap.isKinematic = false;
+
+        }
+    }
+
+
+}
